d2452e9c940742e3c9ab857bc14443205b500658
Xing Zheng <zhengxing@rock-chips.com>
2021-07-15 16:58:25 +0800

    eq_drc_process: increase the version to "1.27 20210715"
    Signed-off-by: Xing Zheng <zhengxing@rock-chips.com>
    Change-Id: Ic3dbcdeaa17356301b940b55883446d84b25ab08

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 #include "Rk_wake_lock.h"
 #include "Rk_socket_app.h"
 
-#define EQ_DRC_PROCESS_VERSION      "1.26 20210704"
+#define EQ_DRC_PROCESS_VERSION      "1.27 20210715"
 
 #define SOC_IS_RK3308           (0x1)
 #define SOC_IS_RK3326           (0x2)
